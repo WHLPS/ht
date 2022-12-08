bf3f5ba002135ebca7a2011958dab61f50789dff
Y <120027156+WHLPS@users.noreply.github.com>
2022-12-08 20:42:27 +0800

    2022.12.8

---
+++ 循环语句初步.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+	int line=0;
+		printf("开始打代码\n");
+	while (line<20000) {
+		printf("继续打代码:%d\n",line);
+		line++;
+ 	}
+	if (line>20000){
+		printf("恭喜你完成了打代码的学习\n");
+		 
+	}	 
+		return 0;
+ } 
